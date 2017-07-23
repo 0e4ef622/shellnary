efa06aa92e06e14322d5342b28d4c363e67966ff
Matthew Tran <0e4ef622@gmail.com>
2017-07-23 10:07:00 -0500

    add big endian

--- shellnary.sh
+++ shellnary.sh
@@ -274,6 +274,21 @@ read_lei64() {
     echo $(( (num[7] << 56) +(num[6] << 48) + (num[5] << 40) + (num[4] << 32) + (num[3] << 24) + (num[2] << 16) + (num[1] << 8) + num[0] ));
 }
 
+read_bei64() {
+    local byte;
+    declare -ai num;
+    for i in {0..7}; do
+        byte='';
+        IFS='' read -r -d '' -n1 byte;
+        if [ -z "$byte" ]; then
+            num[$i]=0;
+        else
+            num[$i]=$(( binaryLookup[$byte] ));
+        fi
+    done
+    echo $(( (num[0] << 56) +(num[1] << 48) + (num[2] << 40) + (num[3] << 32) + (num[4] << 24) + (num[5] << 16) + (num[6] << 8) + num[7] ));
+}
+
 read_leu32() {
     local byte;
     declare -ai num;
@@ -294,6 +309,26 @@ read_lei32() {
     echo $(( num | ~((((num & 0x80000000) >> 31) - 1) | 0xffffffff) ))
 }
 
+read_beu32() {
+    local byte;
+    declare -ai num;
+    for i in {0..3}; do
+        byte='';
+        IFS='' read -r -d '' -n1 byte;
+        if [ -z "$byte" ]; then
+            num[$i]=0;
+        else
+            num[$i]=${binaryLookup[$byte]};
+        fi
+    done
+    echo $(( (num[0] << 24) + (num[1] << 16) + (num[2] << 8) + num[3] ));
+}
+
+read_bei32() {
+    declare -i num=$(read_beu32);
+    echo $(( num | ~((((num & 0x80000000) >> 31) - 1) | 0xffffffff) ))
+}
+
 read_leu16() {
     local byte;
     declare -ai num;
@@ -311,6 +346,26 @@ read_leu16() {
 
 read_lei16() {
     declare -i num=$(read_leu16);
+    echo $(( num | ~((((num & 0x8000) >> 15) - 1) | 0xffff) ))
+}
+
+read_beu16() {
+    local byte;
+    declare -ai num;
+    for i in {0..1}; do
+        byte='';
+        IFS='' read -r -d '' -n1 byte;
+        if [ -z "$byte" ]; then
+            num[$i]=0;
+        else
+            num[$i]=${binaryLookup[$byte]};
+        fi
+    done
+    echo $(( (num[0] << 8) + num[1] ));
+}
+
+read_bei16() {
+    declare -i num=$(read_beu16);
     echo $(( num | ~((((num & 0x8000) >> 15) - 1) | 0xffff) ))
 }
 
